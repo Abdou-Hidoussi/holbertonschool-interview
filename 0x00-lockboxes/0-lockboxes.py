4aa24f00cae6333a172f53b4fcdf544bce98602c
abdou-hidoussi <hidoussiabdou5@gmail.com>
2021-04-21 22:34:41 +0100

    initial

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+def canUnlockAll(boxes):
+
+    new = []
+    for x in boxes[0]:
+        new.append(x)
+
+    for x in range(len(boxes)):
+        if x in new:
+            for y in range(len(boxes[x])):
+                if not boxes[x][y] in new:
+                    new.append(boxes[x][y])
+
+    if len(new) is len(boxes) - 1:
+        return True
+    else:
+        return False
