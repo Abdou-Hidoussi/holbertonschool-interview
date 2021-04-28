12a9cad01b98e7ccff9c38be8eafacb66661ed3c
abdou-hidoussi <hidoussiabdou5@gmail.com>
2021-04-28 17:51:14 +0100

    betty

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -2,36 +2,41 @@
 #include <stdlib.h>
 #include "lists.h"
 
-
+/**
+ * insert_node - insert a new node at a sorted listint_t list
+ * @head: pointer to pointer of first node of listint_t list
+ * @number: integer to be included in new node
+ * Return: address of the new element or NULL if it fails
+ */
 listint_t *insert_node(listint_t **head, int number)
 {
-    listint_t *count, *new, *tmp;
-    
-    if (!*head)
-        return NULL;
-    count = *head;
-    new = malloc(sizeof(listint_t *));
-    if (!new)
-        return NULL;
+	listint_t *count, *new, *tmp;
 
-    new->n = number;
-    while (count)
-    {
-        if (count->next && number < count->next->n)
-        {
-            tmp = count->next;
-            count->next = new;
-            new->next = tmp;
-            break;
-        }
+	if (!*head)
+		return (NULL);
+	count = *head;
+	new = malloc(sizeof(listint_t *));
+	if (!new)
+		return (NULL);
 
-        if (count->next)
-            count = count->next;
-        else
-        {
-            count->next = new;
-            break;
-        }
-    }
-    return (new);
+	new->n = number;
+	while (count)
+	{
+		if (count->next && number < count->next->n)
+		{
+			tmp = count->next;
+			count->next = new;
+			new->next = tmp;
+			break;
+		}
+
+		if (count->next)
+			count = count->next;
+		else
+		{
+			count->next = new;
+			break;
+		}
+	}
+	return (new);
 }
