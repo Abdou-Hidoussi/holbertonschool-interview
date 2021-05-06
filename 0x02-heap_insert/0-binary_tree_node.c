e39603220819e98e85317e1fc4a44552ef065c67
abdou-hidoussi <hidoussiabdou5@gmail.com>
2021-05-06 18:39:00 +0100

    betty

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -1,5 +1,11 @@
 #include "binary_trees.h"
-
+/**
+ * binary_tree_node - create new node
+ * @parent: parent of the new node
+ * @value: value to put in new node
+ * Description: create a new node
+ * Return: new node created
+ */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
--- 0x02-heap_insert/1-heap_insert.c
+++ 0x02-heap_insert/1-heap_insert.c
@@ -1,24 +1,33 @@
 #include "binary_trees.h"
-
+/**
+ * smol - return the smallest size of a tree
+ * @parent: parent of the new node
+ * Return: smallest size
+ */
 int smol(heap_t *parent)
 {
-    if (!parent->right || !parent->left)
-        return (0);
+	if (!parent->right || !parent->left)
+		return (0);
 
-    if (parent->left)
-        return smol(parent->left) + 1;
+	if (parent->left)
+		return (smol(parent->left) + 1);
 
-    if (parent->right)
-        return smol(parent->right) + 1;
+	if (parent->right)
+		return (smol(parent->right) + 1);
 
-    return (0);
+	return (0);
 }
 
-
+/**
+ * switchrue - switch the value of the leaf with the parent
+ * @root: parent of the new node
+ * Return: new node created
+ */
 heap_t *switchrue(heap_t **root)
 {
 	heap_t *parent;
 	int tmp;
+
 	parent = *root;
 	while (parent->parent->n < parent->n)
 	{
@@ -27,37 +36,41 @@ heap_t *switchrue(heap_t **root)
 		parent->n = tmp;
 		parent = parent->parent;
 		if (!parent->parent)
-            break; 		
+			break;
 	}
-	return parent;
+	return (parent);
 }
 
-
+/**
+ * heap_insert - create new node
+ * @root: parent of the new node
+ * @value: value to put in new node
+ * Return: new node created
+ */
 heap_t *heap_insert(heap_t **root, int value)
 {
 	heap_t *parent;
-	parent = *root;
 
+	parent = *root;
 	if (!parent)
 	{
 		*root =  binary_tree_node(parent, value);
-		return *root;
+		return (*root);
 	}
 
 	if (!parent->left)
 	{
 		parent->left = binary_tree_node(parent, value);
-		return switchrue(&parent->left);
+		return (switchrue(&parent->left));
 	}
 	if (!parent->right)
 	{
 		parent->right = binary_tree_node(parent, value);
-		return switchrue(&parent->right);
+		return (switchrue(&parent->right));
 	}
 
 	if (smol(parent->left) <= smol(parent->right))
 		return (heap_insert(&parent->left, value));
 	else
 		return (heap_insert(&parent->right, value));
-	
 }
