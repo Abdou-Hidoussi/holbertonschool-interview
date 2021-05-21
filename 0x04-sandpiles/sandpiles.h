ee7e089bb3bb49e54a5826fa81f673603823bcc8
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2021-05-21 11:10:20 +0100

    initial

---
+++ 0x04-sandpiles/sandpiles.h
@@ -0,0 +1,13 @@
+
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdlib.h>
+#include <stdio.h>
+
+void print_grid_sum(int grid1[3][3], int grid2[3][3]);
+void print_grid(int grid[3][3]);
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+void pop(int grid[3][3], int i, int j);
+void fill(int grid1[3][3], int grid2[3][3]);
+
+#endif
