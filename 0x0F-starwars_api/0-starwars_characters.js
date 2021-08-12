a3bc80d0f5a175674b97eb6ba70f144c92f92933
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2021-08-12 16:48:58 +0100

    await

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -4,14 +4,17 @@ const request = require('request');
 const movieId = process.argv[2];
 const url = `https://swapi.dev/api/films/${movieId}/`;
 
-request(url, function (error, response, body) {
+request(url, async (error, response, body) => {
   if (movieId == null) return;
   if (error) console.log(error);
 
   for (let i = 0; i < JSON.parse(body).characters.length; i++) {
-    request(JSON.parse(body).characters[i], function (error, response, body) {
-      if (error) console.log(error);
-      console.log(JSON.parse(body).name);
+    await new Promise((resolve, reject) => {
+      request(JSON.parse(body).characters[i], function (error, response, body) {
+        if (error) console.log(error);
+        console.log(JSON.parse(body).name);
+        resolve();
+      });
     });
   }
 });
