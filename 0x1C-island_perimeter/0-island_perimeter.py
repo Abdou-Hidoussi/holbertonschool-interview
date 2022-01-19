5bc1e9f6d7bb8d30c541eda00705e0e6385c6e2b
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2022-01-19 22:52:18 +0100

    test

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -2,11 +2,12 @@
 
 """ Function to find perimiter of an island """
 
+
 def island_perimeter(grid):
     """ Function to find perimiter of an island """
     count = 0
     for x in range(len(grid)):
-        for y in range(len(grid)):
+        for y in range(len(grid[x])):
             if grid[x][y] == 1:
                 count += 2
                 break;
@@ -15,7 +16,7 @@ def island_perimeter(grid):
         new_grid.append(grid[i][::-1])
     grid = new_grid
     for x in range(len(grid)):
-        for y in range(len(grid)):
+        for y in range(len(grid[x])):
             if grid[x][y] == 1:
                 count += 2
                 break;
