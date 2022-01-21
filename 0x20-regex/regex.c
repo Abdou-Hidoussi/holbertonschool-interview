faa88548fd8a4756f46fc00ddd8e62ccfa122367
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2022-01-21 09:31:49 +0100

    test

--- 0x20-regex/regex.c
+++ 0x20-regex/regex.c
@@ -8,15 +8,16 @@
  */
 int wildcmp(char const  *s1, char const  *s2)
 {
-
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
-	if (*s2 == '.')
+	if (*s1 == *s2 && *(s2 + 1) != '*')
 		return (wildcmp(s1 + 1, s2 + 1));
-	if (*s1 == *s2)
+	if (*s2 == '.' && *(s2 + 1) != '*')
 		return (wildcmp(s1 + 1, s2 + 1));
 	if (*s2 == '*')
 		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	if (*(s2 + 1) == '*')
+		return (wildcmp(s1, s2 + 2) || wildcmp(s1 + 1, s2));
 	return (0);
 }
 /**
